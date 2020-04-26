91b8ead53bb428448b41ad0451302c1670cc4074
Michaël Lebeau <mikaleb@live.fr>
2020-04-26 13:55:00 +0200

    composables

--- composables/use-movie-api.ts
+++ composables/use-movie-api.ts
@@ -1,14 +1,30 @@
 import { reactive, ref, toRefs, computed } from '@vue/composition-api'
 
-export default function useMovieApi () {
-  const tvId: string = '456-the-simpsons'
+export default function useMovieApi (ctx, apiSelected: string = 'TMDB') {
   const nbSeasons: number = 31
+  let tvId: string = ''
+  let endpoint: string = ''
+
+  const _loading = ref(false)
+  const state = reactive({
+    error: null,
+    loading: computed(() => _loading.value),
+    promise: null,
+    result: null
+  })
+
+  if (apiSelected === 'TMDB') {
+    tvId = '456-the-simpsons'
+    endpoint = 'https://api.themoviedb.org/3/tv/'
+  }
 
   const getAllRatings = async () => {
-
+    const ip = await ctx.$axios.$get('http://icanhazip.com')
+    this.ip = ip
   }
 
   return {
-    getAllRatings
+    getAllRatings,
+    ...toRefs(state)
   }
 }
