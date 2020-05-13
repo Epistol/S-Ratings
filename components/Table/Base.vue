6dd0a8660bfadfbe2e7c736f1ed6ad27853b9192
Michaël Lebeau <michael.lebeau@abbd.fr>
2020-05-14 00:00:44 +0200

    feat(vote): wip vote

--- components/Table/Base.vue
+++ components/Table/Base.vue
@@ -12,18 +12,26 @@
               </template>
             </tr>
           </thead>
-          <tbody>
-            <template v-for="(item,key) in items">
+          <tbody v-if="cSeasons">
+            <template v-for="(ratings,key) in episodesRatings">
               <tr :key="key">
-                <th>{{ item.episodeNb + 1 }}</th>
-                <template v-for="(data, index) in item">
-                  <cell
-                    v-if="index !== 'episodeNb'"
-                    :key="index"
-                    :data="data"
-                    :index="index"
-                    :episode="item.episodeNb + 1"
-                  ></cell>
+                <th>{{ ratings.episodeNb + 1 }}</th>
+                <template v-for="(episodeVotes, season) in ratings">
+                  {{episodeVotes}}
+                  <!-- <v-tooltip :key="season" v-model="dialog" top>
+                    <template v-slot:activator="{ on }">
+                       <cell
+                        v-on="on"
+                        v-if="season !== 'episodeNb'"
+                        :key="season"
+                        :data="{...props.season[season].episodes[ratings.episodeNb], episodeVotes}"
+                        :index="season"
+                        :episodeNb="ratings.episodeNb + 1"
+                        @click="dialog = !dialog"
+                      ></cell>
+                    </template>
+                    <span>Programmatic tooltip</span>
+                  </v-tooltip>-->
                 </template>
               </tr>
             </template>
@@ -35,9 +43,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, computed } from '@vue/composition-api'
+import { defineComponent, onMounted, computed, ref } from '@vue/composition-api'
 import HeadCell from '~/components/Table/HeadCell.vue'
 import Cell from '~/components/Table/Cell.vue'
+import useMovieApi from '@/composables/use-movie-api'
 
 export default defineComponent({
   name: 'tableBase',
@@ -56,7 +65,9 @@ export default defineComponent({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setup(props, ctx) {
     const cSeasons = computed(() => props.seasons)
+    console.log('setup -> cSeasons', cSeasons)
     const cMaxEpisodesNb = computed(() => props.maxNbEpisodesPerSeason)
+    const apiSelected = process.env.API_CHOICE ? process.env.API_CHOICE : 'TMDB'
 
     let loading: boolean = false
 
@@ -81,29 +92,32 @@ export default defineComponent({
     }
 
     // For each episode number, we set all the ratings of each season
-    const setRatingsPerEpisodeNb = (episode: number = 0) => {
+    // Because v-table can't do otherwise
+    const setRatingsPerEpisodeNb = () => {
       loading = true
-      let episodeNb = episode
-      const ratingsObj = []
-      // for each episode Nb, we will load the ratings of the episode NB from all seasons
-      for (episodeNb; episodeNb <= cMaxEpisodesNb.value; episodeNb++) {
-        const episodeObj: any = { episodeNb }
+      const episodesObj: any = []
+      // ex : [{episodeNb : 0}, {episodeNb: 1}]
+      for (
+        let episodeNb = 0;
+        episodeNb <= props.maxNbEpisodesPerSeason;
+        episodeNb++
+      ) {
+        episodesObj.push({ episodeNb })
 
-        props.seasons.map((season: any) => {
-          episodeObj[season.season_number] = season.votesAvg[episodeNb]
-            ? season.votesAvg[episodeNb]
-            : null
-        })
-
-        ratingsObj.push({
-          episodeObj,
+        // Set all votes of episodes for a season
+        cSeasons.value.map((season: any) => {
+          // If the season episodes contain the episode number, add it to the episode Nb array
+          episodesObj[episodeNb][season.season_number] = season.episodes.filter(
+            (episode: any) => episode.episode_number === episodeNb + 1
+          )
         })
       }
 
       loading = false
-      return ratingsObj.map((obj: any) => {
-        return obj.episodeObj
-      })
+      return episodesObj
+      // return ratingsObj.map((obj: any) => {
+      //   return obj.episodeObj
+      // })
     }
 
     const getColor = (rating: number) => {
@@ -128,9 +142,27 @@ export default defineComponent({
     }
 
     setHeaders()
-    const items = setRatingsPerEpisodeNb()
+    const episodesRatings = setRatingsPerEpisodeNb()
+    console.log('setup -> episodesRatings', episodesRatings)
 
-    return { getColor, headers, items, loading, cSeasons }
+    let dialog = ref<boolean>(false)
+    const toggleDialog = async (episodeNb: any) => {
+      const { getEpisodeInfos } = useMovieApi({ ctx, apiSelected })
+      dialog.value = !dialog.value
+      // if (episodeNb && dialog.value === true) {
+      //   apiData = await getEpisodeInfos(cellIndex.value, props.episodeNb)
+      // }
+    }
+
+    return {
+      getColor,
+      headers,
+      episodesRatings,
+      loading,
+      cSeasons,
+      dialog,
+      toggleDialog,
+    }
   },
 })
 </script>
--- components/Table/Cell.vue
+++ components/Table/Cell.vue
@@ -1,43 +1,11 @@
 <template>
-  <v-dialog v-model="dialog" scrollable width="80%" class="overflow-y-hidden">
-    <template v-slot:activator="{ dialog }">
-      <td
-        v-if="cellData === null"
-        class="p-1 whitespace-no-wrap text-gray-900"
-        @click="toggleDialog()"
-      ></td>
-      <td
-        v-else
-        class="p-1 whitespace-no-wrap text-gray-900"
-        :class="cellData !== undefined ? getColor(cellData) : '' "
-        @click="toggleDialog()"
-      >{{ cellData !== undefined ? cellData.toFixed(1) : '' }}</td>
-    </template>
-    <!-- <v-card
-      v-if="cellData.infos"
-      @click="toggleDialog()"
-      :id="cellData.infos.id"
-      class="overflow-y-hidden"
-    >
-      <v-row justify="space-between">
-        <v-col cols="4">
-          <v-img
-            class="white--text align-end"
-            contain
-            :src="'https://image.tmdb.org/t/p/w500/' + cellData.infos.poster_path"
-          ></v-img>
-        </v-col>
-
-        <v-col cols="8" class>
-          <v-card-text class="text--primary" max-height="300px">
-            <div v-if="cellData.infos.overview">{{cellData.infos.overview}}</div>
-            <div v-else>No infos provided yet</div>
-          </v-card-text>
-        </v-col>
-      </v-row>
-    </v-card>-->
-  </v-dialog>
-</template>
+  <!-- <td v-if="cellData === null" class="p-1 text-gray-900 whitespace-no-wrap"></td>
+  <td
+    v-else
+    class="p-1 text-gray-900 whitespace-no-wrap"
+    :class="cellData !== undefined ? getColor(cellData) : '' "
+  >{{ cellData !== undefined ? cellData.toFixed(1) : '' }}</td>
+</template> -->
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from '@vue/composition-api'
@@ -58,39 +26,30 @@ export default defineComponent({
   },
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setup(props, ctx) {
-    const apiSelected = process.env.API_CHOICE ? process.env.API_CHOICE : 'TMDB'
-
     const cellData = ref<any>(props.data)
+    console.log("setup -> cellData", cellData.value)
     const cellIndex = ref<any>(props.index)
     let apiData = reactive({})
-    let dialog = ref<boolean>(false)
 
-    const toggleDialog = async () => {
-      const { getEpisodeInfos } = useMovieApi({ ctx, apiSelected })
-      dialog.value = !dialog.value
-      if (props.episodeNb && dialog.value === true) {
-        apiData = await getEpisodeInfos(cellIndex.value, props.episodeNb)
-      }
-    }
     const getColor = (rating: number) => {
       if (rating === 0) {
         return 'bg-gray-500'
       }
       if (rating < 7) {
-        return 'bg-red-500'
+        return 'hover:bg-gray-600  cursor-pointer bg-red-500'
       }
       if (rating < 8) {
-        return 'bg-orange-500'
+        return 'hover:bg-gray-600  cursor-pointer bg-orange-500'
       }
       if (rating < 8.4) {
-        return 'bg-yellow-500'
+        return 'hover:bg-gray-600  cursor-pointer bg-yellow-500'
       }
       if (rating >= 8) {
-        return 'bg-green-500'
+        return 'hover:bg-gray-600  cursor-pointer bg-green-500'
       }
     }
 
-    return { cellData, cellIndex, dialog, toggleDialog, getColor }
+    return { cellData, cellIndex, getColor }
   },
 })
 </script>
